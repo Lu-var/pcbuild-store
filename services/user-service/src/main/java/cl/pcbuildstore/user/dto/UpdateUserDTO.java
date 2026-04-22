3906450f3e40bb167568df58affe602adff2c908
Lucas Vargas <luc.vargasp@duocuc.cl>
2026-04-22 18:46:17 -0400

    Add Lombok annotations to UpdateUserDTO

--- services/user-service/src/main/java/cl/pcbuildstore/user/dto/UpdateUserDTO.java
+++ services/user-service/src/main/java/cl/pcbuildstore/user/dto/UpdateUserDTO.java
@@ -1,7 +1,15 @@
 package cl.pcbuildstore.user.dto;
 
 import cl.pcbuildstore.user.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateUserDTO {
     private String name;
     private String lastName;
