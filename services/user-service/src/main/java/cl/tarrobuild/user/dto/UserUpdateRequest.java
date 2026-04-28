1785a1a8a01a5ec0b46b681fb5e8e3858fc19336
luvar <luvargaspanza@gmail.com>
2026-04-27 23:19:54 -0400

    refactor: convert UserRequest, UserResponse, and UserUpdateRequest to records

--- services/user-service/src/main/java/cl/tarrobuild/user/dto/UserUpdateRequest.java
+++ services/user-service/src/main/java/cl/tarrobuild/user/dto/UserUpdateRequest.java
@@ -1,23 +1,7 @@
 package cl.tarrobuild.user.dto;
 
-import cl.tarrobuild.user.model.User;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserUpdateRequest {
-    private String name;
-    private String lastName;
-    private String phone;
-
-    public UserUpdateRequest(User user) {
-        this.name = user.getName();
-        this.lastName = user.getLastName();
-        this.phone = user.getPhone();
-    }
-}
+public record UserUpdateRequest(
+        String name,
+        String lastName,
+        String phone
+) {}
